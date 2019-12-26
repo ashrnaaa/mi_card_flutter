9fc2715bb825f425ac568abce195afc85e08ad24
ashrnaaa <ashrnaaa@outlook.com>
2019-12-26 16:17:47 +0100

    Git initial update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.pink,
+          body: Center(child: Container()),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
